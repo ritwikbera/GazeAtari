ff666b8a020aaf22ac76a0e5a0ad6b246d83c958
ritwikbera <ritwikbera12@gmail.com>
2020-05-02 15:18:22 -0500

    added visualization

--- panda.py
+++ panda.py
@@ -20,40 +20,72 @@ def crop_image(img,tol=0):
     row_start,row_end = mask1.argmax(),m-mask1[::-1].argmax()
     return img[row_start:row_end,col_start:col_end]
 
+def white_to_red(im):
+	data = np.array(im)   # "data" is a height x width x 4 numpy array
+	red, green, blue, alpha = data.T # Temporarily unpack the bands for readability
+
+	# Replace white with red... (leaves alpha values alone...)
+	white_areas = (red == 255) & (blue == 255) & (green == 255)
+	data[..., :-1][white_areas.T] = (255, 0, 0) # Transpose back needed
+
+	im2 = Image.fromarray(data)
+	return im2
+
 files = glob.glob('dataset/alien/*.txt')
 episode = 0
 filename = files[episode]
 img_folder = 'dataset/alien/extracted/'+filename.split('/')[-1][:-4]
 out_folder = 'dataset/alien/gaze/'+filename.split('/')[-1][:-4]
 os.makedirs(out_folder, exist_ok=True)
+viz_folder = 'dataset/alien/viz/'+filename.split('/')[-1][:-4]
+os.makedirs(viz_folder, exist_ok=True)
 print(filename)
 print(img_folder)
 print(out_folder)
 
 assert os.path.isdir(img_folder) and os.path.isdir(out_folder)
 
+i = 0
 with open(filename, 'r') as read_obj:
 	csv_reader = DictReader(read_obj)
 	header = next(csv_reader)
 	if header != None:
 		for row in csv_reader:
-			row['gaze_positions'] = row[None] + [row['gaze_positions']]
-			del row[None]
-			
+			try:
+				row['gaze_positions'] = row[None] + [row['gaze_positions']]
+				del row[None]
+			except KeyError:
+				continue  # No None means just gaze pixel present, skip this frame
+
 			# print(row)
 			
 			frame_file = img_folder+'/'+row['frame_id']+'.png'
 			assert os.path.isfile(frame_file)
 			frame = io.imread(frame_file)
-			frame = crop_image(frame)
+
+			# frame = crop_image(frame)
+
 			gazemap = np.zeros_like(frame)[:,:,0]
+			assert gazemap.shape == frame.shape[:-1]
+
 			for coord in row['gaze_positions']:
 				x,y = coord.strip().split('.')
-				x,y = int(x), int(y)
+				x,y = np.clip(int(x),0,gazemap.shape[0]-1), np.clip(int(y),0,gazemap.shape[1]-1)
 				gazemap[x,y] = 255
 
-			io.imsave(out_folder+'/'+row['frame_id']+'.png', gazemap)
+			out_file = out_folder+'/'+row['frame_id']+'.png'
+			io.imsave(out_file, gazemap)
 			
+			im1 = Image.open(frame_file)
+			im2 = Image.open(out_file)
+			im1 = im1.convert('RGBA')
+			im2 = im2.convert('RGBA')
+			im2 = white_to_red(im2)
+			blend = Image.blend(im1, im2, alpha=0.5)
+			blend.save(viz_folder+'/img_{:05}.png'.format(i))
+			i = i + 1
+			
+			# blend.show()
 			# print(gazemap.shape)
 			# plt.imshow(gazemap)
 			# plt.imshow(frame)
