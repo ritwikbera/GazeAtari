computation graph fixes, sandbox script, data parallelism working, progress bar

eval.py
@@ -16,7 +16,7 @@ train_loader, size = get_loader(config)
 model = init_model(train_loader)
 writer = create_summary_writer(config, model, train_loader)
 
-cp_path = 'trial3.pth'
+cp_path = 'checkpoint/trial.pth'
 model.load_state_dict(torch.load(cp_path))
 model.eval()
 
@@ -26,6 +26,8 @@ def print_param(model):
 			print(param, param[1].numel())
 			writer.add_histogram(param[0], param[1].detach().squeeze().numpy(), bins='auto')
 # print(list(model.parameters()))
+
+print_param(model)
 
 for i, batch in enumerate(train_loader):
 	# if i%1000 > 0:

graphLayer.py
@@ -27,8 +27,23 @@ class GCU(Module):
         self.outfeatures = outfeatures
         self.batch = batch
         self.W = Parameter(torch.Tensor(d,V))
+        self.W.requires_grad = True
+        self.W.retain_grad()
         self.variance = Parameter(torch.Tensor(d,V))
-        self.weight = Parameter(torch.Tensor(d, outfeatures))
+        self.variance.requires_grad = True
+        self.variance.retain_grad()
+        self.weight = Parameter(torch.ones(d, outfeatures))
+        self.weight.requires_grad = True
+        self.device = 'cpu'
+
+        # torch.nn.init.xavier_uniform_(self.weight)
+        torch.nn.init.xavier_uniform_(self.W)
+        torch.nn.init.xavier_uniform_(self.variance)
+        # print(self.weight)
+
+        # self.weight.register_hook(self.save_grad('weight_g'))
+        # self.W.register_hook(self.save_grad('W'))
+        # self.variance.register_hook(self.save_grad('variance'))
 
         self.iden = torch.eye(self.d)
         self.iden = torch.cat((self.iden, self.iden))
@@ -38,7 +53,14 @@ class GCU(Module):
         
         self.count = 0
         self.initialized = False
-        
+        self.grads = {}
+
+    def save_grad(self, name):
+        def hook(grad):
+            self.grads[name] = grad
+            assert not (grad == torch.zeros(grad.size())).all()
+            # print(name, grad.size(), (grad == torch.zeros(grad.size())).all())
+        return hook
 
     def init_param(self,x):
 
@@ -72,28 +94,29 @@ class GCU(Module):
 
         # print(W, variance)
 
-        self.W, self.variance = W, variance
-
-        # return W, variance
+        # self.W, self.variance = W, variance
 
     def GraphProject_optim(self, X):
 
-        Adj = torch.Tensor(self.batch, self.no_of_vert, self.no_of_vert)
-        Z = torch.Tensor(self.batch, self.d, self.no_of_vert)
-        Q = torch.Tensor(self.batch, self.ht*self.wdth, self.no_of_vert)
+        Adj = torch.Tensor(self.batch, self.no_of_vert, self.no_of_vert).to(self.device)
+        Z = torch.Tensor(self.batch, self.d, self.no_of_vert).to(self.device)
+        Q = torch.Tensor(self.batch, self.ht*self.wdth, self.no_of_vert).to(self.device)
 
         X = torch.reshape(X,(self.batch, self.ht*self.wdth, self.d))
         
-        zero = torch.Tensor(X.shape).fill_(0)
+        zero = torch.Tensor(X.shape).fill_(0).to(self.device)
         new = torch.cat((zero, X), dim=2)
 
-        extend = torch.matmul(new, self.iden)
-        # print(extend.size())
+        extend = torch.matmul(new, self.iden.to(self.device))
 
         W = torch.reshape(self.W, (self.d*self.no_of_vert,))
         variance = torch.reshape(self.variance, (self.d*self.no_of_vert,))
 
+        # W.register_hook(self.save_grad('W'))
+        # variance.register_hook(self.save_grad('variance'))
+
         q = extend - W[None, None, :]
+
 
         q1 = (q/variance[None, None, :])
         q = q1**2
@@ -101,9 +124,14 @@ class GCU(Module):
         q = torch.sum(q, dim=2)
         q = torch.reshape(q, (self.batch, self.ht*self.wdth, self.no_of_vert))
         Q = q
+
+        # q.register_hook(self.save_grad('q'))
         
         Q = Q.refine_names('B','HW','V')
-        Q -= torch.min(Q, 2)[0].align_as(Q)
+        Q -= torch.min(Q, 'V')[0].align_as(Q) # backprop bottleneck point
+
+        # Q.register_hook(self.save_grad('Q'))
+
         Q = torch.exp(-Q*0.5)
         norm = torch.sum(Q, dim='V')
         Q = torch.div(Q, norm.align_as(Q))
@@ -118,11 +146,16 @@ class GCU(Module):
         z = torch.sum(z, dim='HW')
         z = torch.add(z, 10e-8)/torch.add(torch.sum(Q,dim='HW'), 10e-8)
 
+        # z.register_hook(self.save_grad('z'))
+
         norm = torch.sum(z**2, dim='d')
         Z = torch.div(z,norm.align_as(z))
         Z = Z.rename(None)
 
         Adj = torch.matmul(torch.transpose(Z,1,2), Z)
+
+        # Z.register_hook(self.save_grad('Z'))
+        # Adj.register_hook(self.save_grad('Adj'))
 
 
         return (Q, Z, Adj)
@@ -138,6 +171,9 @@ class GCU(Module):
         return X_new.unflatten('HW', (('H',self.ht),('W',self.wdth))).rename(None)
 
     def forward(self, X):
+        self.batch = X.size(0)
+        self.device = 'cpu' if X.get_device() == -1 else 'cuda:{}'.format(X.get_device())
+
         if not self.initialized:
             self.init_param(X[0]) # one input from batch
             self.initialized = True
@@ -146,11 +182,13 @@ class GCU(Module):
     
         Q, Z, Adj = self.GraphProject_optim(X)
 
-        # self.weight = self.weight.refine_names('d','d_out').align_as
         out = torch.matmul(torch.transpose(Z,1,2), self.weight[None,:,:])
         out = torch.matmul(Adj, out)
         Z_o = F.relu(out)
         Z_o = Z_o.refine_names('B','V','d_out')
+
+        # Z_o.register_hook(self.save_grad('Z_o'))
+
         out = self.GraphReproject(Z_o, Q).permute(0,3,1,2) # permute channel dimension
 
         return out

model.py
@@ -5,21 +5,22 @@ import torch
 
 class GazePred(nn.Module):
 
-    def __init__(self): # sample batch BCHW
+    def __init__(self, V=8, outfeatures=12): # sample batch BCHW
         super(GazePred, self).__init__()
         self.layers_set = False
+        self.V = V
+        self.outfeatures = outfeatures
 
     def set_layers(self, sample_batch):
         # self.conv1 = nn.Conv2d(in_channels=sample_batch.size(1), out_channels=16, kernel_size=8, stride=4, padding=2)
         # self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1)
         
         bs,h,w,ch = sample_batch.size()
-        self.gc3 = GCU(V=8, h=h, w=w, d=ch, batch=bs)
+        self.gcu = GCU(V=self.V, h=h, w=w, d=ch, batch=bs, outfeatures=self.outfeatures)
 
-        gcu_out_ch = 1
         output_dim = 2
 
-        self.conv = nn.Conv2d(in_channels=gcu_out_ch, out_channels=output_dim, kernel_size=3, padding=(1,1))
+        self.conv = nn.Conv2d(in_channels=self.outfeatures, out_channels=output_dim, kernel_size=3, padding=(1,1))
         self.conv_fc1 = nn.Conv2d(in_channels=h, out_channels=1, kernel_size=1) 
         self.conv_fc2 = nn.Conv2d(in_channels=w, out_channels=1, kernel_size=1)
 
@@ -28,8 +29,9 @@ class GazePred(nn.Module):
         if not self.layers_set:
             self.set_layers(x)
             self.layers_set = True
-
-        y = self.gc3(x)
+        x = (x-127.5)/127.5
+        y = self.gcu(x)
+        y = y + x.permute(0,3,1,2) # residual connection
         y = self.conv(y)
         y = y.rename('B','C','H','W')
         # FC layers fully replaced by 2 fully conv layers
@@ -40,7 +42,7 @@ class GazePred(nn.Module):
         return y
 
 if __name__=='__main__':
-    img = torch.randn(2,16,16,3)
+    img = torch.randn(2,16,16,3*4)
     model = GazePred()
     out = model(img)
     print(out.size())

train.py
@@ -28,14 +28,17 @@ def run(config):
     writer = create_summary_writer(config, model, train_loader)
 
 
-    model = ModelPrepper(model, config).out
+    model, device = ModelPrepper(model, config).out
     if config['optimizer']=='Adam':
         optimizer = Adam(model.parameters(), lr=config['lr'])
     else:
         optimizer = Rprop(model.parameters())
 
-    if config['mode'] == 'eval':
+    if config['mode'] == 'eval' or config['resume'] == 1:
         model.load_state_dict(torch.load(config['ckpt_path']))
+
+    desc = "ITERATION - loss: {:.2f}"
+    pbar = tqdm(initial=0, leave=False, total=size, desc=desc.format(0))
 
     def process_batch(engine, batch):
         frames, outputs = depickle(batch)
@@ -44,13 +47,16 @@ def run(config):
             model.train()
             preds = model(frames)
             optimizer.zero_grad()
-            loss = loss_fn(outputs, preds)*100
+            loss = loss_fn(outputs.to(device), preds)*100
             loss.backward()
             optimizer.step()
+            # print(model.gcu.W.grad)
+            # print(model.gcu.weight.grad)
+            # print(model.gcu.variance.grad)
         elif config['mode'] == 'eval':
             model.eval()
             preds = model(frames)
-            loss = loss_fn(outputs, preds)*100
+            loss = loss_fn(outputs.to(device), preds)*100
         else:
             raise NotImplementedError
 
@@ -58,7 +64,7 @@ def run(config):
         # print(pred.size())
 
         # print(preds, outputs)
-        print(loss.item())
+        # print(loss.item())
 
         return loss.item()
 
@@ -79,6 +85,9 @@ def run(config):
 
     @trainer.on(Events.ITERATION_COMPLETED)
     def tb_log(engine):
+        pbar.desc = desc.format(engine.state.output)
+        pbar.update(config['batch_size'])
+
         if config['mode'] in ['train','overfit']:
             writer.add_scalar('training/avg_loss', engine.state.metrics['loss'] ,engine.state.iteration)
         else:
@@ -86,19 +95,14 @@ def run(config):
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def print_trainer_logs(engine):
+        pbar.refresh()
         avg_loss = engine.state.metrics['loss']
-        print('Trainer Results - Epoch {} - Avg loss: {:.2f}'.format(engine.state.epoch, avg_loss))
-    
-    trainer.run(train_loader, max_epochs=config['epochs'], epoch_length=size/config['batch_size'])
+        tqdm.write('Trainer Results - Epoch {} - Avg loss: {:.2f}'.format(engine.state.epoch, avg_loss))
+        pbar.n = pbar.last_print_n = 0
 
+    trainer.run(train_loader, max_epochs=config['epochs'], epoch_length=size/config['batch_size'])
+    pbar.close()
+    
 if __name__ == "__main__":
-    config = {'game':'alien', 
-              'batch_size':4, 
-              'epochs':40, 
-              'lr':5e-3, 
-              'optimizer':'Adam',
-              'log_interval':100, 
-              'n_gpu':2,
-              'mode':'overfit',   
-              'ckpt_path':'trial1.pth'} # 'train' or 'eval' or 'overfit'
+    config = json.load(open('config.txt'))
     run(config)